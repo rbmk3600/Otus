977b63469b8623f6ff333804000d6d154c7dfaa3
rbmk3600 <alvk.klimov@gmail.com>
2025-05-30 08:28:16 +0300

    [add] Суммируем два числа, заранее заданного размера

--- big_numbers.asm
+++ big_numbers.asm
@@ -1,29 +1,40 @@
 
 ; You may customize this and other start-up templates; 
 ; The location of this template is c:\emu8086\inc\0_com_template.txt
-
+  
+N = 4  
 org 100h
+
 MOV SI, OFFSET NUM1
 MOV BX, OFFSET NUM2
 
+MOV CX, N
+
 MOV DI, OFFSET RES
 
-PUSH SI
-lodsb      ; IN AL BYTE WE HAVE READ
-MOV AH, AL ; SAVE IT TO BL 
-MOV SI, BX ;
-LODSB      ; NUM2
-MOV BX, SI
- 
-POP SI     ; POINT TO num1
-ADD AL, AH ; 
-STOSB
+Summ:
+    
+    LODSB      ; 
+    PUSH SI    ; Save SI for NUM1        
+    MOV AH, AL ; Save AL for NUM1
+    
+    MOV SI, BX ; Set BI to NUM2 
+    LODSB      ; 
+    MOV BX, SI ; Save SI for NUM2
+     
+    POP SI     ; Restore SI to point to NUM1
+    
+    
+    ADD AL, AH ;Sum NUM1 and NUM2 bytes and store them to RES
+    STOSB
+    
+LOOP Summ
 
-
-
-num1 DB 0x05, 0x07, 0x09
-num2 DB 0x02, 0x04, 0x06
-RES DB 3 DUB(0x00)
+JMP $
+num1 DB 0x05, 0x07, 0x09, 0x0A
+num2 DB 0x02, 0x04, 0x06, 0x01 
+;       0x07  0x0B  0x0F, 0x0B
+RES DB N DUB(0x00)
 ret
 
 
